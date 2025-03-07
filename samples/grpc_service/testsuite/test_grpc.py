58cff83559518ffdde8377f42e0453bff17d1144
antonyzhilin <antonyzhilin@userver.tech>
2025-03-07 13:21:41 +0300

    feat testsuite: add substitution_vars commit_hash:f5beb5a1b09058da77310400e27ca9735dd42cc7

--- samples/grpc_service/testsuite/test_grpc.py
+++ samples/grpc_service/testsuite/test_grpc.py
@@ -8,7 +8,7 @@ import samples.greeter_pb2 as greeter_protos  # noqa: E402, E501
 # /// [grpc client test]
 async def test_grpc_client_mock_say_hello(service_client, mock_grpc_greeter):
     @mock_grpc_greeter('SayHello')
-    async def _mock_say_hello(request, context):
+    async def mock_say_hello(request, context):
         return greeter_protos.GreetingResponse(
             greeting=f'Hello, {request.name} from mockserver!',
         )
@@ -17,10 +17,8 @@ async def test_grpc_client_mock_say_hello(service_client, mock_grpc_greeter):
     assert response.status == 200
     assert 'text/plain' in response.headers['Content-Type']
     assert response.text == 'Hello, tests from mockserver!'
-    assert _mock_say_hello.times_called == 1
-
-
-# /// [grpc client test]
+    assert mock_say_hello.times_called == 1
+    # /// [grpc client test]
 
 
 # /// [grpc client test response stream]
@@ -29,7 +27,7 @@ async def test_grpc_client_mock_say_hello_response_stream(
     mock_grpc_greeter,
 ):
     @mock_grpc_greeter('SayHelloResponseStream')
-    async def _mock_say_hello_response_stream(request, context):
+    async def mock_say_hello_response_stream(request, context):
         message = f'Hello, {request.name}'
         for i in range(5):
             message += '!'
@@ -50,10 +48,8 @@ Hello, Python!!!!
 Hello, Python!!!!!
 """
     )
-    assert _mock_say_hello_response_stream.times_called == 1
-
-
-# /// [grpc client test response stream]
+    assert mock_say_hello_response_stream.times_called == 1
+    # /// [grpc client test response stream]
 
 
 # /// [grpc client test request stream]
@@ -62,7 +58,7 @@ async def test_grpc_client_mock_say_hello_request_stream(
     mock_grpc_greeter,
 ):
     @mock_grpc_greeter('SayHelloRequestStream')
-    async def _mock_say_hello_request_stream(request_iterator, context):
+    async def mock_say_hello_request_stream(request_iterator, context):
         message = 'Hello, '
         async for request in request_iterator:
             message += f'{request.name}'
@@ -75,10 +71,8 @@ async def test_grpc_client_mock_say_hello_request_stream(
     assert response.status == 200
     assert 'text/plain' in response.headers['Content-Type']
     assert response.text == 'Hello, Python!!!'
-    assert _mock_say_hello_request_stream.times_called == 1
-
-
-# /// [grpc client test request stream]
+    assert mock_say_hello_request_stream.times_called == 1
+    # /// [grpc client test request stream]
 
 
 # /// [grpc client test streams]
@@ -87,7 +81,7 @@ async def test_grpc_client_mock_say_hello_streams(
     mock_grpc_greeter,
 ):
     @mock_grpc_greeter('SayHelloStreams')
-    async def _mock_say_hello_streams(request_iterator, context):
+    async def mock_say_hello_streams(request_iterator, context):
         message = 'Hello, '
         async for request in request_iterator:
             message += f'{request.name}'
@@ -107,10 +101,8 @@ Hello, Python!!
 Hello, Python!!!
 """
     )
-    assert _mock_say_hello_streams.times_called == 1
-
-
-# /// [grpc client test streams]
+    assert mock_say_hello_streams.times_called == 1
+    # /// [grpc client test streams]
 
 
 @pytest.mark.skipif(
@@ -122,9 +114,7 @@ async def test_say_hello(grpc_client):
     request = greeter_protos.GreetingRequest(name='Python')
     response = await grpc_client.SayHello(request)
     assert response.greeting == 'Hello, Python!'
-
-
-# /// [grpc server test]
+    # /// [grpc server test]
 
 
 # /// [grpc server test response stream]
@@ -134,9 +124,7 @@ async def test_say_hello_response_stream(grpc_client):
     async for response in grpc_client.SayHelloResponseStream(request):
         message += '!'
         assert response.greeting == message
-
-
-# /// [grpc server test response stream]
+        # /// [grpc server test response stream]
 
 
 # /// [grpc server test request stream]
@@ -144,9 +132,7 @@ async def test_say_hello_request_stream(grpc_client):
     request = (greeter_protos.GreetingRequest(name=name) for name in ['Python', '!', '!', '!'])
     response = await grpc_client.SayHelloRequestStream(request)
     assert response.greeting == 'Hello, Python!!!'
-
-
-# /// [grpc server test request stream]
+    # /// [grpc server test request stream]
 
 
 # /// [grpc server test streams]
@@ -156,6 +142,4 @@ async def test_say_hello_streams(grpc_client):
     async for response in grpc_client.SayHelloStreams(requests):
         assert response.greeting == message
         message += '!'
-
-
-# /// [grpc server test streams]
+        # /// [grpc server test streams]
