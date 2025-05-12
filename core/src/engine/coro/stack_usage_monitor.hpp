3c7a2cfd87e91cd07b2a34dafbb3359a6833fe12
antonyzhilin <antonyzhilin@userver.tech>
2025-05-12 19:07:25 +0300

    bug engine: don't hang on 4000+ tasks with stack usage monitor
    A bug was found where creating 4000+ tasks at the same time with stack usage monitor enabled would hang the process in release builds.
    
    As a bandaid, only enable stack usage monitor for no more than 1000 tasks. This means that some stack overflows in high-load services may have poor diagnostics.
    commit_hash:f2fbcea2813caffe437adacd9edd6b7271dc64d8

--- core/src/engine/coro/stack_usage_monitor.hpp
+++ core/src/engine/coro/stack_usage_monitor.hpp
@@ -40,6 +40,10 @@ private:
     utils::FastPimpl<Impl, 1024, 8> impl_;
 };
 
+// It was discovered experimentally that registering more than ~4000 coroutines at the same time may crash or hang
+// the service on Linux x86_64 after UFFDIO_REGISTER failure.
+constexpr std::size_t kStackUsageMonitorLimit = 1000;
+
 std::size_t GetCurrentTaskStackUsageBytes() noexcept;
 
 }  // namespace engine::coro
