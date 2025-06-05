931b90a019de65ccc92de51f436fa57783dde995
kpavlov00 <kpavlov00@yandex-team.com>
2025-06-05 23:00:59 +0300

    fix grpc: refactor client-side Finish wait status handling commit_hash:1262c94a5bbd75d56be8e3adcf16d705d94c3b4b

--- grpc/include/userver/ugrpc/client/impl/async_methods.hpp
+++ grpc/include/userver/ugrpc/client/impl/async_methods.hpp
@@ -86,21 +86,30 @@ void Finish(
     stream.Finish(&status, finish.GetTag());
 
     const auto wait_status = WaitAndTryCancelIfNeeded(finish, state.GetContext());
-    if (wait_status == impl::AsyncMethodInvocation::WaitStatus::kCancelled) {
-        ProcessFinishCancelled(state);
-        // Finish AsyncMethodInvocation will be awaited in its destructor.
-        if (throw_on_error) {
-            throw RpcCancelledError(state.GetCallName(), "Finish");
-        }
-    }
+    switch (wait_status) {
+        case impl::AsyncMethodInvocation::WaitStatus::kOk:
+        case impl::AsyncMethodInvocation::WaitStatus::kError:
+            UINVARIANT(
+                impl::AsyncMethodInvocation::WaitStatus::kOk == wait_status,
+                "Client-side Finish: ok should always be true"
+            );
+            state.GetStatsScope().SetFinishTime(finish.GetFinishTime());
+            ProcessFinish(state, final_response);
+            if (throw_on_error) {
+                CheckFinishStatus(state);
+            }
+            break;
 
-    UINVARIANT(
-        impl::AsyncMethodInvocation::WaitStatus::kOk == wait_status, "Client-side Finish: ok should always be true"
-    );
-    state.GetStatsScope().SetFinishTime(finish.GetFinishTime());
-    ProcessFinish(state, final_response);
-    if (throw_on_error) {
-        CheckFinishStatus(state);
+        case impl::AsyncMethodInvocation::WaitStatus::kCancelled:
+            ProcessFinishCancelled(state);
+            // Finish AsyncMethodInvocation will be awaited in its destructor.
+            if (throw_on_error) {
+                throw RpcCancelledError(state.GetCallName(), "Finish");
+            }
+            break;
+
+        case impl::AsyncMethodInvocation::WaitStatus::kDeadline:
+            UINVARIANT(false, "unreachable");
     }
 }
 
