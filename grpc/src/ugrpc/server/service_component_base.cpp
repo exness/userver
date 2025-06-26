bf18b8ede69f69f9730129dd09bc3b9790d5008f
Fedor Nesterenko <fedor.nesterenko@exness.com>
2025-06-26 15:50:59 +0300

    added support for several gRPC servers in one instance

--- grpc/src/ugrpc/server/service_component_base.cpp
+++ grpc/src/ugrpc/server/service_component_base.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include <string_view>
 #include <userver/ugrpc/server/service_component_base.hpp>
 
 #include <userver/components/component_config.hpp>
@@ -16,14 +18,25 @@
 
 USERVER_NAMESPACE_BEGIN
 
-namespace ugrpc::server {
+namespace {
 
+std::string GetServerName(const components::ComponentConfig& config) {
+    static constexpr std::string_view kServerNameKey = "server-name";
+    if (config.HasMember(kServerNameKey)) {
+        return config[kServerNameKey].As<std::string>();
+    }
+    return "grpc-server";
+}
+
+}  // namespace
+
+namespace ugrpc::server {
 ServiceComponentBase::ServiceComponentBase(
     const components::ComponentConfig& config,
     const components::ComponentContext& context
 )
     : impl::MiddlewareRunnerComponentBase(config, context, MiddlewarePipelineComponent::kName),
-      server_(context.FindComponent<ServerComponent>()),
+      server_(context.FindComponent<ServerComponent>(GetServerName(config))),
       config_(server_.ParseServiceConfig(config, context)),
       info_(ServiceInfo{config.Name()}) {
     config_.middlewares = CreateMiddlewares(*info_);
@@ -52,6 +65,10 @@ properties:
         type: string
         description: the task processor to use for responses
         defaultDescription: uses grpc-server.service-defaults.task-processor
+    server-name:
+        type: string
+        description: the name of the server to use
+        defaultDescription: grpc-server
 )");
 }
 
