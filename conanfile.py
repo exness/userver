061b5e95f7694e7ff3d1f1897168f14ce60c048d
Fedor Nesterenko <fedor.nesterenko@exness.com>
2026-01-15 12:35:06 +0200

    fix version

--- conanfile.py
+++ conanfile.py
@@ -95,7 +95,7 @@ class UserverConan(ConanFile):
                 'version.txt',
             ),
         )
-        hotfix_version = '2-dev'
+        hotfix_version = '2'
         self.version = content.strip() + f".{hotfix_version}"  # pylint: disable=attribute-defined-outside-init
 
     def source(self):
